fcdeed33d1db8eb915b34a738c28ebfb7973a6f9
Antje Witte <antjewitte90@gmail.com>
2023-06-29 12:57:12 +0200

    zweiterCommit

--- pages/api/random-character.js
+++ pages/api/random-character.js
@@ -6,12 +6,16 @@ export default function handler(request, response) {
   const character = {
     firstName: chance.first(),
     lastName: chance.last(),
-    // ... some more keys here ...
+    gender: chance.gender(),
+    age: chance.age(),
+    country: chance.country(),
+    profession: chance.profession(),
+    twitter: chance.twitter(),
   };
 
-  if (!character) {
-    response.status(404).json({ message: `Character  not found.` });
-  }
+  //   if (!character) {
+  //     response.status(404).json({ message: `Character  not found.` });
+  //   }
 
   response.status(200).json(character);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data } = useSWR("/api/random-character");
+  if (!data) {
+    return;
+  }
+  return (
+    <>
+      <h1>Beliebiger Character:</h1>
+      <p>
+        {data.firstName} {data.lastName}
+      </p>
+      <p>{data.gender}</p>
+      <p>{data.age}</p>
+    </>
+  );
 }
